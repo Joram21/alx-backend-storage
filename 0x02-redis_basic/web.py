b759d2b432775fc1afdd68b39b58dc521f7c0840
Joram21 <oloojoram@gmail.com>
2024-01-24 19:46:45 +0300

    redis basics

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,18 @@
 #!/usr/bin/env python3
-"""
-Web file
-"""
-import requests
+# -*- coding: utf-8 -*-
+""" create a cache system with redis """
 import redis
-from functools import wraps
+import requests
 
-store = redis.Redis()
-
-
-def count_url_access(method):
-    """ Decorator counting how many times
-    a Url is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+r = redis.Redis()
 
 
-@count_url_access
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """ get page """
+    count  = 0
+    
+    r.set(f"count:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached: {url}", 10, r.get(f"cached: {url}"))
+    return resp.text
